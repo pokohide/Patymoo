a6993ffe4be504a29a2c938fef7ce5b495175bdf
pokohide <hyde31415@gmail.com>
2017-05-01 03:13:57 +0900

    route定義

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -17,4 +17,9 @@
 
 class Admin < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true,
+    length: { minimum: 6 }, if: -> { new_record? || changes['password'] }
 end
