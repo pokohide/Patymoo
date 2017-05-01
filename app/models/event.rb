2cb0b1161e0168ffdb4e6823f69a42bff58eea72
pokohide <hyde31415@gmail.com>
2017-05-01 18:32:22 +0900

    create Event Model

---
+++ app/models/event.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  admin_id    :integer
+#  name        :string(64)       not null
+#  description :text
+#  link        :string
+#  start_at    :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_events_on_admin_id  (admin_id)
+#
+
+class Event < ApplicationRecord
+end
