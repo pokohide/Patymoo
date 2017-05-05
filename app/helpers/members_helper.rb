eade989be1a6f399839b36d36abe6675d76efd13
pokohide <hyde31415@gmail.com>
2017-05-06 04:24:34 +0900

    facebook

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,26 @@
 module MembersHelper
+
+  def twitter_link tw
+    return '未登録' if tw.blank?
+    twitter = tw.delete('@')
+    link_to "@#{twitter}", "https://twitter.com/#{twitter}",
+      target: "_blank", class: 'twitter-link'
+  end
+
+  def facebook_link fb
+    return '未登録' if fb.blank?
+    link_to fb, "https://www.facebook.com/#{fb}/",
+      target: "_blank", class: 'facebook-link'
+  end
+
+  def mail_link mail
+    return '未登録' if mail.blank?
+    mail_to mail, mail, class: 'mail-link'
+  end
+
+  def connpass_link connpass
+    return '未登録' if connpass.blank?
+    link_to connpass, "https://connpass.com/user/#{connpass}",
+      target: "_blank", class: 'connpass-link'
+  end
 end
