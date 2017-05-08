2b82c629b66af0b5a825958962fa09dec3821d16
pokohide <hyde31415@gmail.com>
2017-05-09 01:16:03 +0900

    event index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,5 +22,17 @@ $(document).ready(() => {
   $('.ui.dropdown').dropdown({ on: 'hover' })
   $('.ui.checkbox').checkbox()
 
+  /* notificationの閉じるをクリックしたら閉じる */
+  $('.ui.message .close.icon').on('click', function() {
+    $(this).parent().fadeOut()
+  })
+
+  /* notificationは何もしなくても10秒後に消滅する */
+  setTimeout(() => {
+    $('.ui.message').each(function() {
+      if(!$(this).attr('data-flash')) $(this).fadeOut('normal')
+    })
+  }, 10000)
+
   // $('img.lazy').lazyload({ effect : "fadeIn" })
 })
