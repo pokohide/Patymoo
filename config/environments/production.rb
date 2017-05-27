7a6061413cc2ce5c8411b0f8758a97ee6c6a19e5
pokohide <hyde31415@gmail.com>
2017-05-28 03:14:42 +0900

    メールの設定　

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,22 +25,21 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  # config.action_mailer.default_url_options = { host: 'https://beeapp2016.herokuapp.com' }
+  address = ENV['MAIL_ADDRESS']
+  password = ENV['MAIL_PASSWORD']
+
+  config.action_mailer.default_url_options = { host: 'https://patymoo.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    #:enable_starttls_auto => false,
+    enable_starttls_auto: true,
     address: 'smtp.gmail.com',
-    port: 587,
+    port: '587',
     domain: 'gmail.com',
     authentication: :plain,
-    user_name: 'patymoo.me',
-    password: ENV['MAIL_PASSWORD']
+    user_name: address,
+    password: password
   }
-
-
-
-
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
